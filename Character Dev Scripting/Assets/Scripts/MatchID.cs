da918d46be1f1b6e822bc9be9e92b9c15cafbff0
Becky Trammel <10798648@my.uvu.edu>
2020-01-16 12:19:22 -0700

    Added Unity Project

---
+++ Character Dev Scripting/Assets/Scripts/MatchID.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchID : MonoBehaviour
+{
+	/*private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("fred"))
+		{
+			do work
+		}*/
+
+	public NameID nameIDobj;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		var other.nameID = other.GetComponent<MatchID().nameID
+	}
+}
