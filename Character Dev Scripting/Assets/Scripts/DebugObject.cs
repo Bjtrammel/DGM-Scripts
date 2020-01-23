981f1b7610417b61f80a308d393e1a71e828e505
Becky <10798648@my.uvu.edu>
2020-01-23 12:47:00 -0700

    In Class Animation State Machine

---
+++ Character Dev Scripting/Assets/Scripts/DebugObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+[CreateAssetMenu]
+public class DebugObject : ScriptableObject
+{
+    public void OnDebug(string message)
+    {
+        Debug.Log(message);
+    }
+}
+
+/*[CreateAssetMenu]
+public class GameAction : ScriptableObject
+{
+    public UnityAction action;
+
+    public void Raise()
+    {
+        action?.Invoke();
+    }
+}
+
+[CreateAssetMenu]
+public class FloatData : ScriptableObject
+{
+    public float value = 1f;
+}
+
+[CreateAssetMenu]
+public class FloatData : ScriptableObject
+{
+    public int value = 1;
+}*/ //Doesn't work, script has to have the same name as object
