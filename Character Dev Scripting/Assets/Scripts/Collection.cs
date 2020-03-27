3e26e7e7621ecfa0d784352d739d399379a9678f
Becky <10798648@my.uvu.edu>
2020-03-27 09:06:18 -0600

    If only I could commit as well as I play animal crossing everyday

---
+++ Character Dev Scripting/Assets/Scripts/Collection.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+[CreateAssetMenu]
+public class Collection : ScriptableObject
+{
+    public List<Collectable> collectablesList;
+    public int currentCollectableNum;
+
+    public void AddToCollection(Collectable collectableObj)
+    {
+        foreach (var obj in collectablesList)
+        {
+            if (collectablesList.Contains(collectableObj))
+                return;
+            collectablesList.Add(collectableObj);
+                
+        }
+    }
+    
+    public void RemoveFromCollection(Collectable collectableObj)
+    {
+        for (var index = collectablesList.Count - 1; index >= 0; index--)
+        {
+            var obj = collectablesList[index];
+            if (obj == collectableObj)
+            {
+                collectablesList.Remove(collectableObj);
+            }
+        }
+    }
+
+    public void ClearCollection()
+    {
+        collectablesList.Clear();
+    }
+
+    public void UseCurrentItem()
+    {
+        if (collectablesList.Capacity > 0)
+        {
+            if (collectablesList != null) collectablesList[currentCollectableNum].Use();
+        }
+    }
+
+    public void IncrementCurrentNum()
+    {
+        if (currentCollectableNum > collectablesList.Count - 1)
+        {
+            currentCollectableNum++;
+        }
+        else
+        {
+            currentCollectableNum = 0;
+        }
+    }
+}
--- Character Dev Scripting/Assets/Scripts/DoWork.cs
+++ Character Dev Scripting/Assets/Scripts/DoWork.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DoWork : MonoBehaviour
 {
-    public NameID nameIdobj;
+    public NameId nameIdobj;
 
     public void Work()
     {
